83913fbdcb4633e9593f159139165e4469aca4c8
YO_DI3G0 <diegocda1212@gmail.com>
2025-05-21 21:19:57 -0500

    Update faqConfig.js

--- src/config/faqConfig.js
+++ src/config/faqConfig.js
@@ -7,8 +7,8 @@ export const faqConfig = {
       answer: "Our dedicated team consistently adds new game support. Community suggestions and contributions are also vital to our expansion."
     },
     {
-      question: "What's the whitelisting procedure?",
-      answer: "Upon purchase, your Zerion account is whitelisted instantly. Access details will be sent to your email for immediate platform login."
+      question: "Is Zerion Safe?",
+      answer: "Absolutely! Zerion operates entirely server-side, meaning there are no downloads or modifications to your game client. This ensures your account safety and a fair gaming environment."
     },
     {
       question: "How to avoid account blacklisting?",
@@ -16,23 +16,15 @@ export const faqConfig = {
     },
     {
       question: "Can a blacklist be appealed?",
-      answer: "Blacklist appeals can be made via our support system. Each case is reviewed individually, with decisions based on the violation's nature."
+      answer: "Blacklist appeals can be made via our Discord. Each case is reviewed individually, with decisions based on the violation's nature."
     },
     {
-      question: "What is the Auto Converter?",
-      answer: "The Auto Converter tool helps adapt scripts from other platforms for Zerion, ensuring a seamless transition."
-    },
-    {
-      question: "Tell me about the Partner Program.",
-      answer: "Our Partner Program is for content creators and community influencers, offering benefits like revenue sharing, exclusive features, and direct team support."
-    },
-    {
-      question: "Difference between Standard & Premium plans?",
-      answer: "Standard provides core features at an accessible price. Premium includes all Standard benefits plus exclusive game access, more lenient restrictions, priority support, and a comprehensive package."
+      question: "Can i get banned for using Zerion?",
+      answer: "Since Zerion is fully server-sided and doesn't require any client modification, the risk is significantly lower compared to traditional client-side cheats. However, always be mindful of game developers' terms of service."
     },
     {
       question: "How can I buy Zerion?",
-      answer: "Purchase Zerion directly from our website. We support various payment methods, including credit/debit cards, PayPal, and select cryptocurrencies."
+      answer: "Purchase Zerion directly from our website/discord. We support various payment methods, including credit/debit cards, Cashapp, Etc.."
     }
   ]
 };
